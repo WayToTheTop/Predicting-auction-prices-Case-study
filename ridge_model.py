9d9f752e9ebc0b3e12ff1ac7f748ad4c5f7add27
WayToTheTop <yauheni.koran@gmail.com>
2017-09-06 21:58:14 -0700

    Add auctioneer dummies

--- ridge_model.py
+++ ridge_model.py
@@ -14,20 +14,22 @@ def run(train_address, test_address):
     address - string filepath
     """
 
-    df = pd.read_csv(train_address)
-    df_test = pd.read_csv(test_address)
-    X = get_dummies(df, 'ProductGroup')
-    Y = df['SalePrice'].values
-    X_test = get_dummies(df_test, 'ProductGroup')
-
+    df = pd.read_csv(train_address,
+                parse_dates=['saledate'], infer_datetime_format=True)
+    df_test = pd.read_csv(test_address,
+                parse_dates=['saledate'], infer_datetime_format=True)
+    auct_list = df.auctioneerID.unique()
+    X, y = data_preproccessing(df, auct_list)
+    X_test = data_preproccessing(df_test, auct_list, is_test=True)
     model = ridge_model()
-    error = ourKfold(model, X, Y)
-    train_on_data(df_test, model, X, Y, X_test)
+    error = ourKfold(model, X, y)
+    train_n_predict(df_test, model, X, y, X_test)
     return error
 
 
 def ourLRMSE(y, yhat):
     """Compute Root-mean-squared-error"""
+    yhat[yhat < 0] = 4750
     log_diff = np.log(yhat+1) - np.log(y+1)
     return np.sqrt(np.mean(log_diff**2))
 
@@ -52,14 +54,33 @@ def get_dummies(df, colname):
     """
     colname - string
     """
-    dummies = pd.get_dummies(df[colname]).values
+    dummies = pd.get_dummies(df[colname])
     return dummies
 
-def ridge_model(alpha=0.5):
+def data_preproccessing(df, auct_list, is_test=False):
+    condition = df.YearMade > 1900
+    mode_year = df.YearMade[condition].mode()
+    df.loc[~condition, 'YearMade'] = mode_year.values
+    df['age'] = df.saledate.dt.year - df.YearMade
+    df.auctioneerID.fillna(100., inplace=True)
+    for col in auct_list:
+        df[str(col)] = (df.auctioneerID == col)
+    #Create 5 dummy varibales from ProductGroup
+    X = get_dummies(df, 'ProductGroup')
+    X.drop(labels=X.columns[-1],axis=1,inplace=True)
+    X['age'] = df.age
+    for col in auct_list:
+        X[str(col)] = df[str(col)]
+    if is_test:
+        return X.values
+    y = df['SalePrice']
+    return X.values, y.values
+
+def ridge_model(alpha=0.1):
     ridge = Ridge(alpha=alpha)
     return ridge
 
-def train_on_data(df_test, model, X_train, Y_train, X_test):
+def train_n_predict(df_test, model, X_train, Y_train, X_test):
     model.fit(X_train, Y_train)
     prediction = model.predict(X_test)
     df_test['SalePrice'] = map(int, prediction)
@@ -69,3 +90,4 @@ def train_on_data(df_test, model, X_train, Y_train, X_test):
 
 if __name__ == '__main__':
     print (run('data/Train.csv','data/test.csv'))
+    #X,y = (run('data/Train.csv','data/test.csv'))
